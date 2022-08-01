f64c04c99c5eeea20ba01924c0795fdd6778ca40
Marcelo Pontes <infopontes@gmail.com>
2022-08-01 17:03:49 -0300

    Configurar o método POST da API

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,11 +1,24 @@
 import connect from "../../utils/database"
 
 export default async function handler(req, res) {
-    const { db } = await connect()
-    const response = await db.collection('users').insertOne({
-        name: 'Marcelo Pontes',
-        age: 43,
-    })
-    res.status(200).json(response.name)
+    if (req.method === "POST"){
+        
+        const { nome, idade } = req.body
+
+        if ( !nome || !idade ){
+            res.status(400).json({ message: 'Faltou enviar algum(s) dado(s)' })
+            return
+        }
+
+        const { db } = await connect()
+
+        const response = await db.collection('users').insertOne({
+            nome,
+            idade
+        })
+        res.status(200).json(response.name)
+    }else{
+        res.status(400).json({ message: "Deve usar o método post" })
+    } 
 }
   
